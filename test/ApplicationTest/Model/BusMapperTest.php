aba6afccfe10c79edec9f7de7f39c9669ff9502b
Michael Favila <resourcemode@yahoo.com>
2014-10-24 20:47:03 +0800

    added phpunit.xml

---
+++ test/ApplicationTest/Model/BusMapperTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelfavila
+ * Date: 10/22/14
+ * Time: 12:05 PM
+ */ 
---
+++ test/Bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelfavila
+ * Date: 10/22/14
+ * Time: 10:06 AM
+ */
+
+class Bootstrap extends PHPUnit_Framework_TestCase {
+
+}
+ 
